a1e330ba45778da1ab6fbc747b36cd9be582bf58
cabraham1 <christopherabraham8@gmail.com>
2023-04-10 22:53:23 +0100

    Add css styles

--- src/components/Dragons.jsx
+++ src/components/Dragons.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Planet from '../images/planet.png';
 
 const Dragons = () => (
-  <h1>Dragons</h1>
+  <>
+    <img src={Planet} alt="img" />
+    <h1>Dragons</h1>
+    <p>Dragons are a type of creature that can be found in the game.</p>
+    <button type="button">reserve</button>
+    <button type="button">cancel</button>
+  </>
 );
 
 export default Dragons;
