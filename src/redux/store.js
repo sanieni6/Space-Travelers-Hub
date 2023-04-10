c9ff4e9724bbc0e0a7e4ffd3afc4f1c6528cd76b
sanieni6 <sanieni6@gmail.com>
2023-04-10 15:21:33 -0500

    Add new slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
+import rocketsSlice from './rockets/rocketsSlice';
 
 export const store = configureStore({
   reducer: {
+    rockets: rocketsSlice,
   },
 });
