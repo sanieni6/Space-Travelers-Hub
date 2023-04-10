584f97ee0e4c2ec9a07f383d707767cd49de1c62
sanieni6 <sanieni6@gmail.com>
2023-04-10 15:22:32 -0500

    Add new components, add image resourse for the example

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Rocket from './Rocket';
 
-const Rockets = () => (
-  <h1>Rockets</h1>
-);
+const Rockets = () => {
+  const { rockets } = useSelector((store) => store.rockets);
+  return (
+    <div>
+      <ul className="rocket-list">
+        {rockets.map((rocket) => (
+          <Rocket
+            key={rocket.id}
+            name={rocket.rocket_name}
+            description={rocket.description}
+            image={rocket.flickr_images}
+          />
+
+        ))}
+
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
