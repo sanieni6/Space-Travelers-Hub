28671ef3ae07c2bb2e3966b8c9391866d24dda1d
johnie261 <njorogejohn261@gmail.com>
2023-04-11 01:19:46 +0300

    Import mission reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
+import missionReducer from './ mission/missionSlice';
 
 export const store = configureStore({
   reducer: {
+    mission: missionReducer,
   },
 });
