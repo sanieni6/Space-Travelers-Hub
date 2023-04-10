b5fa98de0284b5312f894d9c4670e92df3c6f831
cabraham1 <christopherabraham8@gmail.com>
2023-04-10 22:19:47 +0100

    solve conflit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import App from './App';
-<<<<<<< HEAD
-import './styles/index.css';
-=======
-import reportWebVitals from './reportWebVitals';
 import './styles/index.scss';
->>>>>>> c61757e549a7f27e71872d4bcf8887d30fd89905
 
 const container = document.getElementById('root');
 const root = createRoot(container);
