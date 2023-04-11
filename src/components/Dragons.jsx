96614a8be0d563faa5f0fb8180ba004c5de51e19
cabraham1 <christopherabraham8@gmail.com>
2023-04-11 16:15:06 +0100

    Implent the fetch api

---
+++ src/components/Dragons.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Dragon from './Dragon';
+import { getDragons } from '../redux/dragon/dragonSlice';
+
+const Dragons = () => {
+  const dragons = useSelector((store) => store.dragon.dragons);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDragons());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <ul className="dragon-list">
+        {dragons.map((dragon) => (
+          <Dragon
+            key={dragon.id}
+            name={dragon.name}
+            type={dragon.dragon_type}
+            description={dragon.description}
+            image={dragon.flickr_images}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Dragons;
