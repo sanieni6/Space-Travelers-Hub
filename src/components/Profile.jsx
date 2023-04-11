101f9ce5648cbd080792bd850a2dd05d6539ca30
cabraham1 <christopherabraham8@gmail.com>
2023-04-12 00:58:06 +0100

    Render dragon on profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import RenderJoined from './renderJoined';
 import RenderDragon from './renderDragon';
+import '../styles/App.css';
 
 const Profile = () => (
-  <>
+  <div className="profile-contanier">
     <RenderJoined />
     <RenderDragon />
-  </>
+  </div>
 );
 
 export default Profile;
