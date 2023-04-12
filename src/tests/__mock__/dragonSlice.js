d692bb0ebf6f65660ec3984409c213c0a56de84c
cabraham1 <christopherabraham8@gmail.com>
2023-04-12 17:02:15 +0100

    Add mock dragon slice

---
+++ src/tests/__mock__/dragonSlice.js
@@ -0,0 +1,31 @@
+const getDragon = () => {
+  const Dragon = [
+    {
+      id: 1,
+      name: 'Dragon 1',
+      type: 'Capsule',
+      description:
+        'The Dragon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Dragon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
+      reserved: true,
+    },
+    {
+      id: 2,
+      name: 'Dragon 2',
+      type: 'Capsule',
+      description:
+        'The Dragon 2 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Dragon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
+      reserved: false,
+    },
+    {
+      id: 3,
+      name: 'Dragon 3',
+      type: 'Capsule',
+      description:
+        'The Dragon 3 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Dragon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
+      reserved: true,
+    },
+  ];
+  return Dragon;
+};
+
+export default getDragon;
