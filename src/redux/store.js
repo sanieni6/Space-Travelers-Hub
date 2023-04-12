cdf8ea7c58034f97fbae89b611f6bf74985e431e
sanieni6 <sanieni6@gmail.com>
2023-04-12 10:34:22 -0500

    Add redux-logger to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger'
 import rocketsReducer from './rockets/rocketsSlice';
 import missionReducer from './mission/missionSlice';
-import dragonReducer from './dragon/dragonSlice'; // Update to import dragonReducer instead of dragonSlice
+import dragonReducer from './dragon/dragonSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
     mission: missionReducer,
-    dragon: dragonReducer, // Update to include dragonReducer
+    dragon: dragonReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
